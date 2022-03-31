9962700c320e08b4fc7bf9dd315af21f6a1bfd28
varunk122 <varunk@iitk.ac.in>
2022-04-01 03:13:57 +0530

    Add efficient algorithm to delete particles

--- generation.hpp
+++ generation.hpp
@@ -31,10 +31,19 @@ struct Domain {
 std::vector<Particle> particles;
 
 std::vector<std::vector<std::vector<int>>> grid;
+// std::unordered_map<int,std::unordered_set<std::pair<Particle,Particle>>> distance_list;
 
 double calculateSphereVolume(double& r) {
 	return (4.0*PI*pow(r,3)) / 3.0;
 }
+
+// void generateDistanceList(std::vector<Particle>& particles) {
+// 	for(int i =0; i< particles.size(); i++) {
+// 		for(int j=0; j< particles.size();j++) {
+// 			double overlap_ = calculateOverlap(particle)
+// 		}
+// 	}
+// }
 
 double calculatePackingFraction(std::vector<Particle>& particles, Domain & dom) {
 	double spheres_vol_ = 0;
@@ -55,6 +64,20 @@ void updatePackingFraction(double& packing_fraction, Domain& dom, Particle& dele
 
 }
 
+void randomDeleting(std::vector<Particle>& particles, Domain& dom, double required_pf) {
+	int total = particles.size();
+	double current_pf = calculatePackingFraction(particles, dom);
+	while(current_pf - required_pf > 0 && total) {
+		int random_particle = std::rand() % total;
+		updatePackingFraction(current_pf, dom, particles[random_particle]);
+		// particles.erase(particles.begin() + random_particle);
+		std::swap(particles[random_particle], particles.back());
+		particles.pop_back();
+		total--;
+	}
+}
+
+
 void printParticlesList(std::vector<Particle>& particles) {
 	
 	for (int i =0; i< particles.size();i++) {
@@ -74,28 +97,32 @@ void saveToCSV(std::vector<Particle>& particles) {
 
 }
 
-double calculateOverlap(Point3d& point1, Point3d& point2) {
+double calculateOverlap(Particle& particle1, Particle& particle2) {
+	Point3d point1 = particle1.centre;
+	Point3d point2 = particle2.centre;
 	double dist_ = sqrt(pow((point1(0) - point2(0)),2) + pow((point1(1) - point2(1)),2) + pow((point1(2) - point2(2)),2));
-	return dist_ - 0.4;
+	return dist_ - (particle1.radius + particle2.radius);
 }
 
 void deleteParticles(std::vector<Particle>& particles) {
 	std::vector<unsigned> toDelete_;
 	for (int i =0; i < particles.size(); i++) {
 		for (int j =i+1; j <particles.size(); j++) {
-			double overlap_ = calculateOverlap(particles[i].centre, particles[j].centre);
-			if( overlap_ < 0 && fabs(overlap_)/0.4 > 0.2) {
-				particles.erase(particles.begin() + j);
+			double overlap_ = calculateOverlap(particles[i], particles[j]);
+			if( overlap_ < 0 ) {
+				// particles.erase(particles.begin() + j);
+				// j--;
+				std::swap(particles[j], particles.back());
+				particles.pop_back();
 				j--;
 			}
 		}
-
 	}
 }
 
 void randomParticleGenerator(std::vector<Particle>& particles, Domain& dom) {
 	int id_ = 0;
-	int raidus_ = 0.2;
+	double raidus_ = 0.05;
 	float packingFraction_ = 0;
 	double simulationVolume_ = (dom.second(0)- dom.first(0))*(dom.second(1)- dom.first(1))*(dom.second(2)- dom.first(2));
 	double spheresVolume_ =0;
@@ -108,10 +135,12 @@ void randomParticleGenerator(std::vector<Particle>& particles, Domain& dom) {
 	std::uniform_real_distribution<double> distribution_z (dom.first(2), dom.second(2));
 
 	while (packingFraction_ < 10) {
-		particles.push_back(Particle(id_++, Point3d(distribution_x(generator), distribution_y(generator), distribution_z(generator)),0.2));
-		spheresVolume_ += 0.0334933;
+		particles.push_back(Particle(id_++, Point3d(distribution_x(generator), distribution_y(generator), distribution_z(generator)),raidus_));
+		spheresVolume_ += calculateSphereVolume(raidus_);
 		packingFraction_ = spheresVolume_/simulationVolume_;
 	}
+
+	std::cout << "Total Particles added into simulation domain: " << particles.size() << std::endl;
 }
 
 
